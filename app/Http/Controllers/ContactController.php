02330e5e0801d29bfbbb819d72ec38804b79fec5
Renan Salustiano <renansalustiano2020@gmail.com>
2023-07-19 14:27:22 -0300

    Criação do controller responsável por gerenciar as requisições de contrato

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ContactService;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * @var ContactService
+     */
+
+    private $contactService;
+
+    public function __construct(ContactService  $contactService)
+    {
+        $this->contactService = $contactService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $vars = array();
+
+        $contacts = $this->contactService->getContacts();
+
+        $vars['contacts'] = $contacts;
+
+        return view('contacts.listing', $vars);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $vars = array();
+        $vars['title'] = 'Novo contato';
+        return view('contacts.form', $vars);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       //todo cadastrar contato
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
